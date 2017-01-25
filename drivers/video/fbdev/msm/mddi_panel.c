09d7d258277a85964b23b218c1077ab8df58b549
Rudolf Tammekivi <rtammekivi@gmail.com>
2017-01-25 12:28:01 +0200

    video: fbdev: Add MSM display driver
    This is initially imported from LA.AF.1.1_rb1.17, but all the legacy code is
    removed and added proper DT support. Also removed duplicate platform devices
    and made proper parent to device structure, so PM is managed automatically.
    
    Change-Id: I3a2bb754abdb7924276feb6c3ed85c20fa1ff043

---
+++ drivers/video/fbdev/msm/mddi_panel.c
@@ -0,0 +1,186 @@
+
+/*
+ * MDDI Generic Panel
+ * Copyright (C) 2016  Rudolf Tammekivi <rtammekivi@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+#define DEBUG
+#include <linux/module.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/pm_runtime.h>
+
+#include "msm_fb.h"
+#include "mddi.h"
+#include "mddihost.h"
+#include "mddihosti.h"
+
+enum mddi_panel_table {
+	PANEL_ON,
+	PANEL_OFF,
+	PANEL_INIT,
+	PANEL_MAX
+};
+
+struct mddi_panel_driver_data {
+	u32 *panel_table[PANEL_MAX];
+	size_t panel_table_size[PANEL_MAX];
+};
+
+static void mddi_write_commands(struct mddi_panel_driver_data *data,
+				enum mddi_panel_table index)
+{
+	int i;
+	u32 *table = data->panel_table[index];
+
+	for (i = 0; i < data->panel_table_size[index]; i += 2) {
+		if (table[i] == 0xff) {
+			mddi_wait(table[i+1]);
+			continue;
+		}
+		mddi_queue_register_write(table[i], table[i+1], true, 0);
+	}
+}
+
+static const char *const dt_table_match[] = {
+	[PANEL_ON] = "panel-on",
+	[PANEL_OFF] = "panel-off",
+	[PANEL_INIT] = "panel-init",
+};
+
+static int mddi_populate_tables(struct device *dev,
+				struct mddi_panel_driver_data *data)
+{
+	int i;
+	struct device_node *node = dev->of_node;
+	int ret;
+
+	for (i = 0; i < PANEL_MAX; i++) {
+		size_t size = of_property_count_elems_of_size(node,
+							      dt_table_match[i],
+							      sizeof(u32));
+		if (size < 1)
+			continue;
+
+		data->panel_table_size[i] = size;
+		data->panel_table[i] = devm_kcalloc(dev, size, sizeof(u32),
+						    GFP_KERNEL);
+		ret = of_property_read_u32_array(node, dt_table_match[i],
+						 data->panel_table[i], size);
+		if (ret) {
+			dev_dbg(dev, "failed to read '%s' ret=%d\n",
+				dt_table_match[i], ret);
+			devm_kfree(dev, data->panel_table[i]);
+			data->panel_table_size[i] = 0;
+			continue;
+		}
+	}
+	return 0;
+}
+
+static int mddi_panel_probe(struct platform_device *pdev)
+{
+	struct device *dev = &pdev->dev;
+	struct device_node *node = dev->of_node;
+	struct mddi_panel_driver_data *data;
+	int ret;
+
+	struct msm_panel_info pinfo;
+
+	data = devm_kzalloc(dev, sizeof(*data), GFP_KERNEL);
+	if (!data)
+		return -ENOMEM;
+
+	ret = mddi_populate_tables(dev, data);
+	if (ret)
+		return ret;
+
+	memset(&pinfo, 0, sizeof(pinfo));
+
+	/* TODO: read from dt */
+	pinfo.dev = dev;
+	pinfo.xres = 480;
+	pinfo.yres = 800;
+	pinfo.type = MDDI_PANEL;
+	pinfo.pdest = DISPLAY_1;
+	pinfo.mddi.vdopkt = MDDI_DEFAULT_PRIM_PIX_ATTR;
+	pinfo.mddi.is_type1 = true;
+	pinfo.bpp = 16;
+	pinfo.lcd.refx100 = 5400;
+	pinfo.lcd.v_back_porch = 6;
+	pinfo.lcd.v_front_porch = 0;
+	pinfo.lcd.v_pulse_width = 0;
+	pinfo.lcd.rev = 1;
+	pinfo.lcd.vsync_enable = true;
+	pinfo.lcd.hw_vsync_mode = true;
+	pinfo.lcd.vsync_notifier_period = (1 * HZ);
+	pinfo.clk_rate = 192000000;
+	pinfo.clk_min = 192000000;
+	pinfo.clk_max = 192000000;
+	pinfo.fb_num = MSM_FB_NUM;
+
+	mddi_write_commands(data, PANEL_INIT);
+
+	platform_set_drvdata(pdev, data);
+	pm_runtime_enable(dev);
+
+	ret = mddi_panel_register(dev, &pinfo);
+	if (ret) {
+		dev_err(dev, "failed to register panel\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static int mddi_panel_runtime_suspend(struct device *dev)
+{
+	struct mddi_panel_driver_data *data = dev_get_drvdata(dev);
+	dev_dbg(dev, "suspending\n");
+
+	mddi_write_commands(data, PANEL_OFF);
+	return 0;
+}
+
+static int mddi_panel_runtime_resume(struct device *dev)
+{
+	struct mddi_panel_driver_data *data = dev_get_drvdata(dev);
+	dev_dbg(dev, "resuming\n");
+
+	mddi_host_client_cnt_reset();
+	mddi_write_commands(data, PANEL_ON);
+	return 0;
+}
+
+static struct dev_pm_ops mddi_panel_dev_pm_ops = {
+	.runtime_suspend = mddi_panel_runtime_suspend,
+	.runtime_resume = mddi_panel_runtime_resume,
+};
+
+static const struct of_device_id mddi_panel_match_table[] = {
+	{ .compatible = "qcom,mddi-panel" },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, mddi_match_table);
+
+static struct platform_driver mddi_panel_driver = {
+	.probe		= mddi_panel_probe,
+	.driver		= {
+		.name	= "mddi-panel",
+		.pm	= &mddi_panel_dev_pm_ops,
+		.of_match_table = mddi_panel_match_table,
+	},
+};
+module_platform_driver(mddi_panel_driver);
